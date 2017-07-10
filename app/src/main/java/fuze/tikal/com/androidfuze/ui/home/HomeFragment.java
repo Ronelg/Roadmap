4f164a04d808dab6784608752b0bb6edab34adb8
shaulr <cul8erm8>
2017-07-10 13:39:36 +0300

    login dialog and github oauth implementation

--- app/src/main/java/fuze/tikal/com/androidfuze/ui/home/HomeFragment.java
+++ app/src/main/java/fuze/tikal/com/androidfuze/ui/home/HomeFragment.java
@@ -6,14 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import fuze.tikal.com.androidfuze.BuildConfig;
 import fuze.tikal.com.androidfuze.R;
+import fuze.tikal.com.androidfuze.data.source.GithubOauth;
 
 
 public class HomeFragment extends Fragment implements HomeContract.View {
-
+    private Button loginButton;
     public static HomeFragment newInstance() {
         return new HomeFragment();
     }
+    public static final String GITHUB_ID = BuildConfig.GITHUB_ID;
+    public static final String GITHUB_SECRET = BuildConfig.GITHUB_SECRET;
 
     @Nullable
     @Override
@@ -21,7 +27,21 @@ public class HomeFragment extends Fragment implements HomeContract.View {
         @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-
+        loginButton = (Button)v.findViewById(R.id.btnLogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GithubOauth
+                        .Builder()
+                        .withClientId(GITHUB_ID)
+                        .withClientSecret(GITHUB_SECRET)
+                        .withContext(getActivity())
+                        .packageName("fuze.tikal.com.androidfuze")
+                        .nextActivity("fuze.tikal.com.androidfuze.ui.home.HomeActivity")
+                        .debug(true)
+                        .execute();
+            }
+        });
         return v;
     }
 
