cbcc98c31678260b3fb1b3732e71feb09108f1cc
Tikal <Gabriel@Tikalk.com>
2017-07-10 12:28:06 +0300

    New features

--- app/src/main/java/fuze/tikal/com/androidfuze/data/Roadmap.java
+++ app/src/main/java/fuze/tikal/com/androidfuze/data/Roadmap.java
@@ -1,10 +1,19 @@
 package fuze.tikal.com.androidfuze.data;
 
 
+import java.util.List;
+
 /**
  * Created by ronelg on 7/10/17.
  */
 
 public class Roadmap {
+
+    public enum Status {PUBLISHED, DRAFT};
+
+    private int version;
+    private Status status;
+
+    private List<Step> steps;
 
 }
